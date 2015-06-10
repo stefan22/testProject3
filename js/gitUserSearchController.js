b05b5ca14078a0f2765de4b1469d66aea71908f3
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-10 21:47:10 +0100

    linked up factory to controller and called it from index.html

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,14 +1,14 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
 
-  var searchResource = $resource('https://api.github.com/search/users');
+
 
   var self = this;
 
   self.doSearch = function() {
-    self.searchResult = searchResource.get(
-        { q: self.searchTerm }
-
-    );
+    Search.query(self.searchTerm)
+      .then(function(response) {
+        self.searchResult = response.data;
+      })
 
   }; //doSearch
 
