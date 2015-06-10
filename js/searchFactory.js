65841001fc8dfa7dc8cdbd45222a14edcacc2cae
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-10 21:39:51 +0100

    added http test and factory js query return function

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,7 +1,14 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
-
+  var queryUrl = 'https://api.github.com/search/users';
   return {
-    query: 'foo'
+    query: function(searchTerm) {
+      return $http({
+        url: queryUrl,
+        method: 'GET',
+        params: {
+          'q': searchTerm
+        }
+      });
+    }
   }
-
 }]);
